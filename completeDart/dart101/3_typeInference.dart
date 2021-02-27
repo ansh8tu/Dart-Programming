308c63456c4705c9c3ae0ef884b13dda6716dbae
ansh8tu <anshu8tu@gmail.com>
2021-02-27 09:47:45 +0530

    type inference in dart!

---
+++ completeDart/dart101/3_typeInference.dart
@@ -0,0 +1,10 @@
+void main() {
+  //remember we used to write string here
+  //but now we don't have to do that as dart infers to the type itself
+  var name = "Ansh";
+  var age = 19;
+
+  print("My name is : $name");
+  print("My name has ${name.length} letters!");
+  print('I\'m $age while i\'m typing this!');
+}
