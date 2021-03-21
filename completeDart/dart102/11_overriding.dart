9970638042155aa96264b60d8196f411ee770203
ansh8tu <anshu8tu@gmail.com>
2021-03-22 03:43:16 +0530

    Added the concept of overriding in dart!

---
+++ completeDart/dart102/11_overriding.dart
@@ -0,0 +1,52 @@
+void main() {
+  final studentOne = Student(name: "James", age: 19);
+
+  //calling the method
+  studentOne.intro();
+
+  // prints
+  //The name of the student is: James and the age is: 19
+
+  final studentPhysics =
+      PhysicsStudent(name: "Sheldon", age: 19, major: "Quantum Mechanics");
+  studentPhysics.intro();
+
+  //prints
+  //The name of the student is: Sheldon and the age is: 19 and the major is : Quantum Mechanics
+
+  //calling the overriden functions
+  // prints Overriding the toString() method in parent class!
+  print(studentOne.toString());
+
+  // prints Overriding the toString() method in child class!
+  print(studentPhysics.toString());
+}
+
+class Student {
+  String name;
+  int age;
+
+  Student({required this.name, required this.age});
+
+  //added a method to print the intro to the screen
+  void intro() =>
+      print("The name of the student is: $name and the age is: $age");
+
+  @override
+  String toString() => "Overriding the toString() method in parent class!";
+}
+
+//use of extends for inheritance
+class PhysicsStudent extends Student {
+  String major;
+
+  // use of super keyword to take the values of memebers from the parent class
+  PhysicsStudent({required String name, required int age, required this.major})
+      : super(name: name, age: age);
+
+  void intro() => print(
+      "The name of the student is: $name and the age is: $age and the major is : $major");
+
+  @override
+  String toString() => "Overriding the toString() method in child class!";
+}
