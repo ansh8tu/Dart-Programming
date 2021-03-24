d9a42972a892760f65a67fc547c96b47b2e7b963
ansh8tu <anshu8tu@gmail.com>
2021-03-25 01:21:24 +0530

    Added the concept of lists!

---
+++ completeDart/dart102/14_lists.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> players = ["Sova", "Killjoy", "Reyna"];
+
+  //lets see few methods of the list
+  players.add("Viper");
+  print(players);
+
+  //prints 4
+  print(players.length);
+
+  //removes killjoy
+  players.removeAt(1);
+  print(players);
+
+  //many methods are available you can check them out as well
+}
